526e7ade72300ab3f8d75cb8209b58500e2bd63a
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2022-12-28 18:35:33 +0300

    I connect the app with mongoDB Databas & I use 3rd party mongoose

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const { result } = require('lodash');
+
+
 
 const app = express();
 
+mongoose.connect('mongodb://127.0.0.1:27017/note-tut')
+    .then((result) => {
+        app.listen(3000, ()=> {console.log('server is active on localhost at 3000')})
+        console.log(`connecting to db`)
+    })
+    .catch((error) => {console.log(error)})
+
+
 app.set('view engine', 'ejs');
 
+// Middleware & Static file.
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'hello', snippet: 'hekfsdhfueb jdfuekjd dfheucbdks lasbcdeufbdksa'},
-        {title: 'welcome', snippet: 'hekfsdhfueb jdfuekjd dfheucbdks lasbcdeufbdksa'},
-        {title: 'welcome', snippet: 'hekfsdhfueb jdfuekjd dfheucbdks lasbcdeufbdksa'}
-    ];
 
-    res.render('index', {title: 'Home', blogs});
+app.get('/', (req, res) => {
+    res.redirect('/blogs')
 });
 app.get('/about', (req, res) => {
     res.status(200).render('about', {title: 'About'});
@@ -25,6 +34,16 @@ app.get('/about-us', (req, res) => {
     res.status(301).redirect('/about');
 });
 
+// Blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+        .then((result) => {
+          res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+})
 app.get('/blog/create', (req, res) => {
     res.render('create', {title: 'Create new Blog'});
 });
@@ -32,5 +51,3 @@ app.get('/blog/create', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404', {title: '404'})
 })
-
-app.listen(3000, ()=> {console.log('server is active on localhost at 3000')})
