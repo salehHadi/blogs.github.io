28f463081c403475a6a08e25f4308cabd8d6fddb
Saleh Hadi Aburah <sasa97977s@hotmail.com>
2022-12-29 14:44:26 +0300

    I finished last MVP of this project, I did MNC

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const Blog = require('./models/blog');
 const { result } = require('lodash');
+const blogRoutes = require('./routes/blogRoutes');
 
 
 
@@ -20,6 +20,7 @@ app.set('view engine', 'ejs');
 
 // Middleware & Static file.
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 
@@ -35,19 +36,9 @@ app.get('/about-us', (req, res) => {
 });
 
 // Blog routes
-app.get('/blogs', (req, res) => {
-    Blog.find().sort({createdAt: -1})
-        .then((result) => {
-          res.render('index', {title: 'All Blogs', blogs: result})
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-})
-app.get('/blog/create', (req, res) => {
-    res.render('create', {title: 'Create new Blog'});
-});
+app.use('/blogs', blogRoutes);
 
+// Page 404
 app.use((req, res) => {
     res.status(404).render('404', {title: '404'})
 })
